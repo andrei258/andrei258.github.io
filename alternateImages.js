240bde967174df7f21ba0e232935d2d69407b4ae
Andrei Balan <andreibalan258@gmail.com>
2023-09-12 00:51:18 +0200

    Alternate 2 images like in the game

---
+++ alternateImages.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const image1 = document.getElementById("image1");
+    const image2 = document.getElementById("image2");
+
+    image1.style.display = "block";
+    image2.style.display = "none";
+
+    function alternateImages() {
+        if (image1.style.display === "block") {
+            image1.style.display = "none";
+            image2.style.display = "block";
+        } else {
+            image1.style.display = "block";
+            image2.style.display = "none";
+        }
+    }
+
+    // Toggle images every 3 seconds
+    setInterval(alternateImages, 3000); // 4 seconds in milliseconds
+});
